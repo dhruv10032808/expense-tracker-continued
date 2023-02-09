6840be3c19fd936683cf820d1037a0578394c594
dhruv10032808 <dhruv.bhanu.1003@gmail.com>
2023-02-09 18:27:28 +0530

    fetching expenses

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Route,Routes } from 'react-router-dom';
 import AuthForm from './components/Auth/AuthForm';
 import EmailVerification from './components/pages/EmailVerification';
 import ForgotPassword from './components/pages/ForgotPassword';
 import HomePage from './components/pages/HomePage';
+import AuthContext from './store/AuthContextProvider';
 
 
 function App() {
+  const authCtx=useContext(AuthContext);
   return <>
   <Routes>
-  <Route path='/' exact element={<AuthForm />}></Route>
-  <Route path='/home' exact element={<HomePage/>}></Route>
-  <Route path='/emailverification' exact element={<EmailVerification/>}></Route>
-  <Route path='/forgotpassword' exact element={<ForgotPassword/>}></Route>
+  {!authCtx.isLoggedIn && <Route path='/' exact element={<AuthForm />}></Route>}
+  {authCtx.isLoggedIn && <Route path='/home' exact element={<HomePage/>}></Route>}
+  {authCtx.isLoggedIn && <Route path='/emailverification' exact element={<EmailVerification/>}></Route>}
+  {authCtx.isLoggedIn && <Route path='/forgotpassword' exact element={<ForgotPassword/>}></Route>}
   </Routes>
   </>
 }
